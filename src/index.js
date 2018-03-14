c84b507aa8acd4a57b23cb2e2f40007cad796418
Jorge Ramón <jorger@dsindigo.mx>
2018-03-14 13:36:51 -0600

    Agregando HTTPS

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ app.use(cors());
 const pokemonRouter = require("./routes/pokemon");
 app.use("/pokemons", pokemonRouter);
 
-app.listen(8080, function() {
-  console.log("Listening port 8080");
-});
+const https = require('https');
+const fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/api.pwa.dsindigo.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/api.pwa.dsindigo.com/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/api.pwa.dsindigo.com/chain.pem')
+};
+
+https.createServer(options, app).listen(80);
