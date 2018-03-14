63f30deae2383722c9b68235274d5ce0661c5fbc
Jorge Ramón <jorger@dsindigo.mx>
2018-03-14 13:57:42 -0600

    Fix en archivos estáticos

--- src/index.js
+++ src/index.js
@@ -6,17 +6,26 @@ require("./config/database");
 const cors = require("cors");
 app.use(cors());
 
+app.use('/static', express.static(__dirname + '/static'));
+
 const pokemonRouter = require("./routes/pokemon");
 app.use("/pokemons", pokemonRouter);
-app.use('/static', express.static('./static'));
 
 const https = require('https');
 const fs = require('fs');
 
-var options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/api.pwa.dsindigo.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/api.pwa.dsindigo.com/cert.pem'),
-  ca: fs.readFileSync('/etc/letsencrypt/live/api.pwa.dsindigo.com/chain.pem')
-};
+if (process.env.NODE_ENV === "https") {
+  var options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/api.pwa.dsindigo.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api.pwa.dsindigo.com/cert.pem'),
+    ca: fs.readFileSync('/etc/letsencrypt/live/api.pwa.dsindigo.com/chain.pem')
+  };
 
-https.createServer(options, app).listen(8080);
+  https.createServer(options, app).listen(8080, function () {
+    console.log('Running on port 8080 with HTTPs');
+  });
+} else {
+  app.listen(8080, function () {
+    console.log('Running on port 8080')
+  });
+}
